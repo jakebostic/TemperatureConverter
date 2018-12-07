e0c36027029954cd4f6df084675513f8c81ea279
jakebostic <jake_bostic@yahoo.com>
2018-12-07 16:40:48 -0500

    Initial Commit

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Temperature Converter");
+            Console.WriteLine();
+
+            Console.Write("Enter degrees in Farenheit: ");
+            if (double.TryParse(Console.ReadLine(), out double f))
+            {
+                double c = (f - 32) * 5 / 9;
+                Console.Write($"\nDegrees in Celsius: {c}");
+                Console.WriteLine();
+            }
+            
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
